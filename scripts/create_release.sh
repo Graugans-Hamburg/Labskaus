7977d3096af8e13197630098f8f685039410555b
Matthias B <brett_vorm_kopf@gmx.de>
2018-01-23 21:01:56 +0100

    Update the release build script so that it does also create a release package for windows

--- scripts/create_release.sh
+++ scripts/create_release.sh
@@ -11,19 +11,22 @@ echo "Die folgende Version wird erstellt: Labskaus_$version"
 ##
 echo 'Step 2:Erstelle einen neuen Ordner auf dem Desktop mit dem Namen der Programmversion'
 Zielordner="$HOME/Desktop/Labskaus_$version"
+Zielordner_Windows="$HOME/Desktop/Labskaus_Windows_$version"
 mkdir $Zielordner
 
 ##
 echo 'Step 3:Kopiere alle help Dateien in den Zielordner'
 cp -R ../help $Zielordner
+cp -R ../help $Zielordner_Windows
 
 ##
 echo 'Step 4:Kopiere das Programm in den Zielordner'
 cp ../bin/Release/Labskaus "$Zielordner"
-
+cp ../bin/Release_Windows/Labskaus.exe "$Zielordner_Windows"
 ##
 echo 'Step 5:Kopiere die zusätzlichen Recourcen in den Zielordner' 
 cp -R ../pic $Zielordner
+cp -R ../pic $Zielordner_Windows
 
 ##
 echo 'Step 6: Kopiere das configure.sh in den Zielordner'
@@ -34,6 +37,8 @@ echo 'Step 7: Das ganze Verzeichnis packen'
 cd $Zielordner
 cd ..
 zip -r "Labskaus_$version".zip "Labskaus_$version"
-
+cd $Zielordner_Windows
+cd ..
+zip -r "Labskaus_Windows_$version".zip "Labskaus_Windows_$version"
 ##
 echo 'Fertig'
